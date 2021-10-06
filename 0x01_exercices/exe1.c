1021b5f3d01df4de99d453f92cfd250d552c8b88
kevin <k.lienard@yahoo.fr>
2021-10-06 14:04:22 +0000

    test

--- 0x01_exercices/exe1.c
+++ 0x01_exercices/exe1.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * Ecrire un programme qui demande un nombre entier a l'utilisateur
+ * et affiche la valeur de la saisie en octal et hexadecimal
+ * 
+ * - Formater en octal > %o
+ * - Formater en hexa > %x ou %X
+ */
+
 int main(void)
 {
     int nombre_entier = 0;
